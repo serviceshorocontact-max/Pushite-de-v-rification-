43c112ae382ddfc31dc86d5b9e201a68a4a043b2
serviceshorocontact-max <serviceshorocontact@gmail.com>
2026-08-01 19:59:59 +0100

    Create tailwin.config.js

---
+++ tailwin.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-white': '#FFFFFF',
+        'custom-green': '#2E7D32',
+        'custom-light-green': '#4CAF50',
+        'custom-yellow': '#FFC107',
+        'custom-light-yellow': '#FFF9C4',
+      },
+    },
+  },
+  plugins: [],
+}
