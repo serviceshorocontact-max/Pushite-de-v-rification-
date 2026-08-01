b58e1f717c3fed1a240cbb027e45e0c9203fc170
serviceshorocontact-max <serviceshorocontact@gmail.com>
2026-08-01 20:10:03 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import TicketForm from '@/app/components/TicketForm'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-4 bg-custom-gradient">
+      <TicketForm />
+    </main>
+  )
+}
