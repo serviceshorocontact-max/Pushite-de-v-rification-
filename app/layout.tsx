019fc6f6fcd9245e98068f3bfa26910e87c9d149
serviceshorocontact-max <serviceshorocontact@gmail.com>
2026-08-02 02:44:29 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Poppins } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/app/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ 
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'] 
+})
 
 export const metadata: Metadata = {
-  title: 'Ticket d\'activation',
-  description: 'Vente de tickets d\'activation',
+  title: 'Ticket d\'activation - Obtenez votre code en 2 min',
+  description: 'Vente de tickets d\'activation sécurisée',
 }
 
 export default function RootLayout({
@@ -17,9 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Navbar />
-        {children}
+        <main className="min-h-screen bg-custom-gradient">
+          {children}
+        </main>
       </body>
     </html>
   )
